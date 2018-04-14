3c46c7a8935177a1c48be2064654d035eb75f412
ecoulson <ivansonofcoul@gmail.com>
2018-04-14 13:27:07 -0700

    working on error handling of insantiation of a PowerQuery Object

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 export default class PowerQuery {
+
+	/**
+	 * 
+	 * @param {Document} document 
+	 */
+	constructor(document) {
+		if (document == null) {
+			let error = new Error('The document argument is null');
+			error.name = 'NullHTMLDocumentError';
+			throw error;
+		}
+
+		let type = typeof document;
 	
+		if (type == 'object') {
+			let error = new Error('The document argument passed is not an object');
+			error.name = 'IllegalTypeError';
+			throw error;
+		}
+
+		this.document = document;
+	}
+
+	getNode(selector) {
+
+	}
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,30 @@
 import PowerQuery from '../src/index';
 const DOM = require('./helpers/helper');
+const {expect} = require('chai');
 
-console.log(PowerQuery, DOM);
+describe('PowerQuery Suite', () => {
+	describe('Instantiation Suite', () => {
+		it('Should Throw A NullDocumentError', () => {
+			try {
+				let x = new PowerQuery(null);
+			} catch(e) {
+				return expect(e.name).to.equal('NullHTMLDocumentError');
+			}
+			expect('Error To Be Thrown').to.equal(true);
+		});
+
+		it('Should Throw An IllegalTypeError', () => {
+			try {
+				let x = new PowerQuery(10);
+			} catch(e) {
+				return expect(e.name).to.equal('IllegalTypeError');
+			}
+			expect('Error To Be Thrown').to.equal(true);
+		});
+
+		if ('Should Instantiate a PowerQuery Object', () => {
+			let x = new PowerQuery(D);
+			console.log(x);
+		});
+	});
+});
